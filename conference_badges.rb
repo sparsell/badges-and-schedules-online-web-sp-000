e6441bd28bf7acfd36aedca0c9b476beb2494de2
Sonja Parsell <hello@sparsell.com>
2020-03-25 10:07:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,13 @@
 # Write your code here.
 #badge_maker should return a formatted badge
 def badge_maker(name)
-
   return "Hello, my name is #{name}."
 end
 
 #batch_badge_creator should return a list of badge messages
 #batch_badge_creator should not hard-code response
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Linus", "Matz"]
+
 def batch_badge_creator(attendees)
 
 end
